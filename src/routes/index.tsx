747d9deefbf29edfcee5635cd30cde784956bc56
Lucas Almeida <lucas_almeidasilva01@hotmail.com>
2021-01-09 14:22:41 -0300

    Added splash screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { View, ActivityIndicator } from 'react-native';
+import React, { useEffect } from 'react';
 
+import SplashScreen from 'react-native-splash-screen';
 import AuthRoutes from './auth.routes';
 import AppRoutes from './app.routes';
 
@@ -9,13 +9,11 @@ import { useAuth } from '../hooks/auth';
 const Routes: React.FC = () => {
   const { user, loading } = useAuth();
 
-  if (loading) {
-    return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        <ActivityIndicator size="large" color="#999" />
-      </View>
-    );
-  }
+  useEffect(() => {
+    if (!loading) {
+      SplashScreen.hide();
+    }
+  }, [loading]);
 
   return user ? <AppRoutes /> : <AuthRoutes />;
 };
